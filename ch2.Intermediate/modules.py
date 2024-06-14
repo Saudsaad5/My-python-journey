29b94ed90f19d42d5b6eaca8d1c257f0ae171c65
Saudsaad5 <hason2002009@hotmail.com>
2024-06-14 18:29:42 +0300

    Statterd a new chapter

---
+++ ch2.Intermediate/modules.py
@@ -0,0 +1,13 @@
+
+
+import Mexamples
+
+result = Mexamples.pi
+result = Mexamples.square(3)
+result = Mexamples.cube(3)
+result = Mexamples.circumference(3)
+result = Mexamples.area(3)
+name = Mexamples.saud
+print(result)
+print(name)
+
