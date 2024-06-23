0502a0e111cb121b1f8ebe5f1e81dc4a80eb4d26
Saudsaad5 <hason2002009@hotmail.com>
2024-06-23 12:52:48 +0300

    Property (Setter/Getter) in python

---
+++ ch2.Intermediate/Property.py
@@ -0,0 +1,38 @@
+class Rectangle:
+    
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        return f"{self._width:.1f}cm"
+
+    @property
+    def height(self):
+        return f"{self._height:.1f}cm"
+
+    @width.setter
+    def width(self, new_width):
+        if new_width > 0:
+            self._width = new_width
+        else:
+            print("Width must be greater than zero")
+
+    @height.setter
+    def height(self, new_height):
+        if new_height > 0:
+            self._height = new_height
+        else:
+            print("Height must be greater than zero")
+
+
+rectangle = Rectangle(5, 6)
+
+
+rectangle.height = 8
+rectangle.width = 7
+
+
+print(rectangle.height)
+print(rectangle.width)
